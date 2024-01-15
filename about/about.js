8c535d364a7e9910d273164bc55c95a744b6e7b3
azizhadiid <azizalhadiid88@gmail.com>
2024-01-15 13:22:52 +0700

    first commit

---
+++ about/about.js
@@ -0,0 +1,3 @@
+document.getElementById('downloadButton').addEventListener('click', function() {
+    window.location.href = 'http://www.example.com/';
+});
---
+++ home.js
@@ -0,0 +1,4 @@
+function goToLink() {
+    // Ganti 'https://www.example.com' dengan URL tujuan Anda
+    window.location.href = 'portfolio/index.html';
+  }
